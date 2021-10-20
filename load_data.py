9916b3fd18c78f3d62a65f7813cb9427960b6ba3
ariana <arianasat@ispras.ru>
2021-10-20 22:48:53 +0400

    add train_test_split

--- load_data.py
+++ load_data.py
@@ -3,6 +3,8 @@ import numpy as np
 import wfdb
 import ast
 import json
+import math
+import random
 
 with open('config.json', 'r')as fin:
     config = json.load(fin)
@@ -42,8 +44,47 @@ def save_per_label(Y, collected_datasets_path):
         sub_df.to_csv(collected_datasets_path + '{}_subdataset.csv'.format(label))
 
 
-labeled_df = discard_other_labels(config['ptb_path']+'ptbxl_database.csv', source='ptb')
-save_per_label(labeled_df, config['collected_datasets_path'])
+def get_train_test_split(label):
+    trusted_folds = [9, 10]
+    Y = pd.read_csv(config['collected_datasets_path'] + '{}_subdataset.csv'.format(label))
+    trusted_df = Y[Y['strat_fold'].isin(trusted_folds)]
+    not_trusted_df = Y[~Y['strat_fold'].isin(trusted_folds)]
+
+    needed_test_size = math.floor(len(Y) * 0.2)
+
+    if needed_test_size < len(trusted_df):
+        random.seed(42)
+        test_indexes = random.sample(range(len(trusted_df)), needed_test_size)
+        remaining_indexes = list(set(list(range(len(trusted_df)))) - set(test_indexes))
+
+        y_test = trusted_df.iloc[test_indexes, :]
+        y_train = pd.concat([trusted_df.iloc[remaining_indexes, :], not_trusted_df])
+
+    else:
+        random.seed(42)
+        test_indexes = random.sample(range(len(not_trusted_df)), needed_test_size - len(trusted_df))
+        remaining_indexes = list(set(list(range(len(not_trusted_df)))) - set(test_indexes))
+
+        y_test = pd.concat([trusted_df, not_trusted_df.iloc[test_indexes, :]])
+        y_train = not_trusted_df.iloc[remaining_indexes, :]
+
+    # adding SR examples
+    sr_df = pd.read_csv(config['collected_datasets_path'] + 'SR_subdataset.csv')
+    trusted_sr_df = sr_df[sr_df['strat_fold'].isin(trusted_folds)]
+    test_indexes = random.sample(range(len(trusted_sr_df)), y_test.shape[0])
+    remaining_indexes = list(set(list(range(len(sr_df)))) - set(test_indexes))
+    train_indexes = random.sample(remaining_indexes, y_train.shape[0])
+
+    y_test = pd.concat([y_test, sr_df.iloc[test_indexes, :]])
+    y_train = pd.concat([y_train, sr_df.iloc[train_indexes, :]])
+
+    x_train = load_raw_data(y_train, config['sampling_rate'], config['ptb_path'])
+    x_test = load_raw_data(y_test, config['sampling_rate'], config['ptb_path'])
+
+    y_train["labels"].replace({"SR": 0, label: 1}, inplace=True)
+    y_test["labels"].replace({"SR": 0, label: 1}, inplace=True)
+
+    return x_train, x_test, y_train["labels"], y_test["labels"]
 
 
 """
@@ -51,12 +92,18 @@ save_per_label(labeled_df, config['collected_datasets_path'])
 stat = json.loads(Y['labels'].value_counts().to_json())
 with open(path + 'sub_ptbxl_stat.json', 'w') as fout:
     json.dump(stat, fout, indent=4)
-"""
 
-"""
 # Load raw signal data
 # i.e. for 'CRBBB' label
 our_label_df = pd.read_csv(config['collected_datasets_path']+'CRBBB_subdataset.csv', index_col='ecg_id')
 X = load_raw_data(our_label_df, config['sampling_rate'], config['ptb_path'])
+
+# Separately save scv files for each label
+labeled_df = discard_other_labels(config['ptb_path']+'ptbxl_database.csv', source='ptb')
+save_per_label(labeled_df, config['collected_datasets_path'])
+
+# Get splits for given label
+# i.e. for 'CRBBB' label
+X_train, X_test, y_train, y_test = get_train_test_split('CRBBB')
 """
 
