de472c2b5b8f6a74b2a0a786589a450a7d34f3fc
Shahahne <shtigranyan@ispras.ru>
2021-10-20 23:54:52 +0400

    fix metric error

--- train.py
+++ train.py
@@ -4,6 +4,8 @@ from tensorflow.keras.callbacks import (ModelCheckpoint, TensorBoard, ReduceLROn
 from tensorflow.keras.metrics import Accuracy, Recall, Precision, SensitivityAtSpecificity
 from model import get_model
 
+from load_data import get_train_test_split
+
 if __name__ == "__main__":
     loss = 'binary_crossentropy'
     lr = 0.001
@@ -17,12 +19,12 @@ if __name__ == "__main__":
                                min_delta=0.00001)]
 
     #add data spliting
-    # X_train, y_train, X_test, y_test = split_data_into_train_and_test()
+    X_train, X_test, y_train, y_test = get_train_test_split('CRBBB')
 
     # If you are continuing an interrupted section, uncomment line bellow:
     #   model = keras.models.load_model(PATH_TO_PREV_MODEL, compile=False)
     model = get_model(2)
-    model.compile(loss=loss, optimizer=opt)
+    model.compile(loss=loss, optimizer=opt, metrics=[Accuracy(), Recall(), Precision()])
     # Create log
     callbacks += [TensorBoard(log_dir='./logs', write_graph=False),
                   CSVLogger('training.log', append=False)]  # Change append to true if continuing training
@@ -34,7 +36,6 @@ if __name__ == "__main__":
                         epochs=70,
                         initial_epoch=0,  # If you are continuing a interrupted section change here
                         callbacks=callbacks,
-                        verbose=1,
-                        metrics=[Accuracy(), Recall(), Precision(), SensitivityAtSpecificity()])
+                        verbose=1)
     # Save final result
     model.save("./final_model.hdf5")
