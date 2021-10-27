c57c0a62d8abaa48d1360d3d8ed5762879c1ac53
Shahahne <shtigranyan@ispras.ru>
2021-10-27 13:02:24 +0400

    add result prediction

---
+++ predict.py
@@ -0,0 +1,17 @@
+from tensorflow.keras.models import load_model
+from tensorflow.keras.optimizers import Adam
+
+from load_data import train_val_test_split, config
+
+path_to_model = config["path_to_model"]
+
+loss = 'categorical_crossentropy'
+lr = 0.001
+opt = Adam(lr)
+
+X_train, X_val,  X_test, y_train, y_val, y_test = train_val_test_split()
+
+model = load_model(path_to_model, compile=False)
+model.compile(loss='binary_crossentropy', optimizer=Adam())
+
+y_score = model.predict(X_train)
