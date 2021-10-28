60e917fdee326425df05cb12fc827f681662ea91
Shahahne <shtigranyan@ispras.ru>
2021-10-28 18:28:46 +0400

    add confusion matrix and metrics per label

--- predict.py
+++ predict.py
@@ -1,8 +1,11 @@
 from tensorflow.keras.models import load_model
 from tensorflow.keras.optimizers import Adam
-from sklearn.metrics import accuracy_score, recall_score, f1_score, precision_score
+from sklearn.metrics import accuracy_score, recall_score, f1_score, precision_score, \
+                            classification_report, confusion_matrix
 import numpy as np
+
 import json
+import pickle
 
 from load_data import train_val_test_split, config
 
@@ -26,11 +29,16 @@ def predict():
     metrics = get_metrics(y_test_labels, y_pred_labels)
     with open('./quality.json', 'w')as f:
         json.dump(metrics, f, indent=4)
-
     return y_pred
 
 
 def get_metrics(y_test, y_pred):
+    label_names = ["1AVB", "CRBBB", "CLBBB", "STACH", "SBRAD", "AFIB"]
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    with open('./confusion_matrix.pickle', 'wb')as f:
+        pickle.dump(confusion_matrix, f)
+    with open('./metrics_per_label.txt', 'w')as f:
+        f.write(classification_report(y_test, y_pred, target_names=label_names))
     return {'accuracy': accuracy_score(y_test, y_pred).round(3),
             'recall_macro': recall_score(y_test, y_pred, average="macro").round(3),
             'recall_micro': recall_score(y_test, y_pred, average="micro").round(3),
@@ -39,3 +47,4 @@ def get_metrics(y_test, y_pred):
             'precision_macro': precision_score(y_test, y_pred, average="macro").round(3),
             'precision_micro': precision_score(y_test, y_pred, average="micro").round(3)}
 
+predict()
