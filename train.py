d787f842b19fdd5dd6623552bbe46e07bb007860
arianasatryan <arianasatryan@gmail.com>
2021-11-02 18:55:10 +0400

    saving multi-class results

--- train.py
+++ train.py
@@ -1,20 +1,20 @@
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import (ModelCheckpoint, TensorBoard, ReduceLROnPlateau,
                                         CSVLogger, EarlyStopping)
-from tensorflow.keras.metrics import CategoricalAccuracy, Recall, Precision
+from tensorflow.keras.metrics import Accuracy, Recall, Precision
 from model import get_model
 from load_data import train_val_test_split, config
 
+train_config = config["training"]
+classification_type = 'multi-label'
 
-if __name__ == "__main__":
-    loss = 'categorical_crossentropy'
-    lr = 0.001
-    batch_size = 64
-    opt = Adam(lr)
+
+def train_model(train_config):
+    optimizer = Adam(train_config["lr"])
     callbacks = [ReduceLROnPlateau(monitor='val_loss',
                                    factor=0.1,
                                    patience=7,
-                                   min_lr=lr / 100),
+                                   min_lr=train_config["lr"] / 100),
                  EarlyStopping(patience=9,  # Patience should be larger than the one in ReduceLROnPlateau
                                min_delta=0.00001)]
 
@@ -23,21 +23,21 @@ if __name__ == "__main__":
 
     # If you are continuing an interrupted section, uncomment line bellow:
     #   model = keras.models.load_model(PATH_TO_PREV_MODEL, compile=False)
-    model = get_model(len(config['labels']))
-    model.compile(loss=loss, optimizer=opt, metrics=[CategoricalAccuracy(), Recall(), Precision()])
+    model = get_model(len(train_config["labels"]))
+    model.compile(loss=train_config["loss"], optimizer=optimizer, metrics=[Accuracy(), Recall(), Precision()])
     # Create log
     callbacks += [TensorBoard(log_dir='./logs', write_graph=False),
                   CSVLogger('training.log', append=False)]  # Change append to true if continuing training
     # Save the BEST and LAST model
-    callbacks += [ModelCheckpoint('./backup_model_last.hdf5'),
-                  ModelCheckpoint('./backup_model_best.hdf5', save_best_only=True)]
+    callbacks += [ModelCheckpoint('./{}_backup_model_last.hdf5'.format(classification_type)),
+                  ModelCheckpoint('./{}_backup_model_best.hdf5'.format(classification_type), save_best_only=True)]
     # Train neural network
     history = model.fit(X_train, y_train,
                         validation_data=(X_val, y_val),
-                        batch_size=batch_size,
-                        epochs=30,
+                        batch_size=train_config["batch_size"],
+                        epochs=train_config["epochs"],
                         initial_epoch=0,  # If you are continuing a interrupted section change here
                         callbacks=callbacks,
                         verbose=1)
     # Save final result
-    model.save("./final_model.hdf5")
+    model.save("./{}_final_model.hdf5".format(classification_type))
